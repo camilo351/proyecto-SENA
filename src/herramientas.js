987ed3c71a4cab7b5cd61563d9ce6b869e54f801
camilo351 <camilo.hernandez.0627@gmail.com>
2024-12-02 10:57:30 -0500

    agregando el archivo herramientas.js

---
+++ src/herramientas.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import $ from "jquery";
+import "datatables.net-dt/css/jquery.dataTables.min.css";
+import "datatables.net";
+
+const DataTableComponent = () => {
+  useEffect(() => {
+    // Inicializar DataTable
+    $("#tablaPrestamos").DataTable();
+    // Limpiar la tabla cuando el componente se desmonte
+    return () => {
+      $("#tablaPrestamos").DataTable().destroy(true);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Registro de Préstamos</h1>
+      <table id="tablaPrestamos" className="display">
+        <thead>
+          <tr>
+            <th>Nombre del Aprendiz</th>
+            <th>Ficha del Curso</th>
+            <th>Fecha de Prestación</th>
+            <th>Fecha de Devolución</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Juan Pérez</td>
+            <td>123456</td>
+            <td>2024-12-01</td>
+            <td>2024-12-15</td>
+          </tr>
+          <tr>
+            <td>María Gómez</td>
+            <td>654321</td>
+            <td>2024-11-25</td>
+            <td>2024-12-05</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DataTableComponent;
